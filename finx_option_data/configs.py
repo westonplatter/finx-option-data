615e7930c6d2be60bcbf28b27b18e948798ce372
westonplatter <westonplatter@gmail.com>
2022-05-06 07:04:39 -0600

    code formatting

--- finx_option_data/configs.py
+++ finx_option_data/configs.py
@@ -5,18 +5,14 @@ from dotenv import dotenv_values
 
 class Config(object):
     def __init__(self, config_full_path: str):
-        self.configs = {
-            **self._get_file_configs(config_full_path),
-            **os.environ
-        }
+        self.configs = {**self._get_file_configs(config_full_path), **os.environ}
 
     def _get_file_configs(self, config_full_path: str):
         if os.path.exists(config_full_path):
             return dotenv_values(config_full_path)
         else:
             return {}
-        
+
     @property
     def bucket_name(self):
         return self.configs.get("BUCKET_NAME", None)
-
