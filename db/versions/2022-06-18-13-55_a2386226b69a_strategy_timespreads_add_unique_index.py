57d720c8e848bf6de7ae5bb92ff315415baa745c
westonplatter <westonplatter@gmail.com>
2022-07-06 23:09:32 -0600

    calendar generation working, with tests!

--- db/versions/2022-06-18-13-55_a2386226b69a_strategy_timespreads_add_unique_index.py
+++ db/versions/2022-06-18-13-55_a2386226b69a_strategy_timespreads_add_unique_index.py
@@ -17,8 +17,13 @@ depends_on = None
 
 
 def upgrade():
-    op.create_index(op.f('ix_strategytimespreads_idfb'), 'strategy_timespreads', ['id_f', 'id_b'], unique=True)
+    op.create_index(
+        op.f('ix_strategy_timespreads_dt_desc_ticker_f_ticker_b'),
+        'strategy_timespreads',
+        ["dt", "desc", "ticker_f", "ticker_b"],
+        unique=True
+    )
 
 
 def downgrade():
-    op.drop_index('ix_strategytimespreads_idfb')
+    op.drop_index('ix_strategy_timespreads_dt_desc_ticker_f_ticker_b')
