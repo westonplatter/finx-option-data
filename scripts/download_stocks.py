55d5567ecda51e2d30948d488edcd677269009ea
westonplatter <westonplatter@gmail.com>
2022-08-03 23:51:31 -0600

    tweak download scripts

--- scripts/download_stocks.py
+++ scripts/download_stocks.py
@@ -1,20 +1,18 @@
 import pandas as pd
-
 from finx_option_data.configs import Config
 
 config = Config(".env.prod")
 
 from finx_option_data.stock_quote_fetch_agent import StockQuoteFetchAgent
+sagent = StockQuoteFetchAgent(
+    polygon_api_key=config.polygon_api_key, engine=config.engine_metrics
+)
 
-# sagent = StockQuoteFetchAgent(
-#     polygon_api_key=config.polygon_api_key, engine=config.engine_metrics
-# )
+from finx_option_data.utils import chunks
 
-# from finx_option_data.utils import chunks
-
-# for dates in chunks(pd.date_range(start="2022-6-3", end="2022-7-1", freq="D"), 20):
-#     for ticker in ["SPY"]:
-#         # for ticker in ["SPY", "USO", "QQQ", "IWM"]:
-#         sd, ed = dates[0], dates[-1]
-#         sagent.ingest_price(ticker, sd, ed)
-#     print(f"\nstart={sd}, end={ed}")
+for dates in chunks(pd.date_range(start="2022-6-3", end="2022-8-1", freq="D"), 20):
+    for ticker in ["SPY"]:
+        # for ticker in ["SPY", "USO", "QQQ", "IWM"]:
+        sd, ed = dates[0], dates[-1]
+        sagent.ingest_price(ticker, sd, ed)
+    print(f"\nstart={sd}, end={ed}")
